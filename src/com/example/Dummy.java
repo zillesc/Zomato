17cdd3f5bf290fadddb93f782754c832713942f5
Craig Zilles <zilles@illinois.edu>
2017-09-15 17:16:10 -0500

    Initial commit.

---
+++ src/com/example/Dummy.java
@@ -0,0 +1,9 @@
+package com.example;
+
+/**
+ * Don't use this class; just a dummy file here so that git will include the src directory for you.
+ * Feel free to delete, after you create your own classes.
+ */
+public class Dummy {
+
+}
---
+++ test/com/example/DummyTest.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import static org.junit.Assert.*;
+
+/**
+ * Also a dummy file.  You can delete this (if you want) after you actually create your own test classes.
+ */
+public class DummyTest {
+
+}
